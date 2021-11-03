8eeb44647d9949e6c392634be7c5d23a173a7a2b
heiitalo <italovieira.008@gmail.com>
2021-11-02 23:44:57 -0300

    [Feature Italo Task: correções no routerLink]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntradaComponent } from './entrada/entrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: "entrar", component: EntradaComponent},
+  {path: "cadastrar", component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
